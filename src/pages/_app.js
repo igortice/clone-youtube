372e9b93b2d79d753a6583417220b7b811f0c4c5
Igor Rocha <igortice@gmail.com>
2021-02-08 22:19:19 -0300

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+function CloneYouTube({ Component, pageProps }) {
+  // eslint-disable-next-line
+  return <Component {...pageProps} />;
+}
+
+export default CloneYouTube;
