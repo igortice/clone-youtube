2dbacb100aaaaac98e4393bed349709cc94325b8
Igor Rocha <igortice@gmail.com>
2021-02-09 21:08:42 -0300

    init config material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+import { Button } from '@material-ui/core';
+
 export default function Home() {
-  return <div>Clone Youtube</div>;
+  return (
+    <div>
+      Clone Youtube
+      <Button variant="outlined" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
 }
